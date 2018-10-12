37434eeb4a5eef7e97a8bc8e5b88936927b5a91d
Bartosz Strachowski <bstrachowski@gmail.com>
2018-10-12 12:37:52 +0200

    Add initial solution to repo

---
+++ CV ASP.NET LECT/CV ASP.NET LECT/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CV_ASP.NET_LECT.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
