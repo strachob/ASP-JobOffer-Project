5042450874230430064ef8e183cf955fc8d6bb9e
Bartosz Strachowski <bstrachowski@gmail.com>
2018-11-16 17:03:29 +0100

    Add applying for job functionality

--- CV ASP.NET LECT/CV ASP.NET LECT/Models/JobApplication.cs
+++ CV ASP.NET LECT/CV ASP.NET LECT/Models/JobApplication.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace CV_ASP.NET_LECT.Models
 {
@@ -9,10 +10,17 @@ namespace CV_ASP.NET_LECT.Models
     {
         public int Id { get; set; }
         public int OfferId { get; set; }
+        [Required]
         public string FirstName { get; set; }
+        [Required]
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
+        [Required]
+        [EmailAddress]
         public string EmailAddress { get; set; }
+        [Required]
+        [MinLength(100)]
+        public string ApplicationDescription { get; set; }
         public bool ContactAgreement { get; set; }
         public string CvUrl { get; set; }
     }
---
+++ CV ASP.NET LECT/CV ASP.NET LECT/Models/JobApplicationApplyView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CV_ASP.NET_LECT.Models
+{
+    public class JobApplicationApplyView : JobApplication
+    {
+        public JobOffer JobOffer { get; set; }
+    }
+}
